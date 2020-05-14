dacb661e25efb60e136654b2cee08e2131b9a7c2
Wesley <wesleyburgener@gmail.com>
2020-05-14 16:53:56 -0600

    Adding my hello line to Emma's code

--- src/dude/Main.java
+++ src/dude/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Emma");
+        System.out.println("Hello Emma, my name is Wes.");
 
     }
 }
